64df370dfe19c4984e94ff2f9a9ab5c5bdf90ccb
Hanyu <8245snake@gmail.com>
2021-06-13 22:13:18 +0900

    設定からモデルを作成する処理を追加

--- Simulator/PharmacokineticModelFactory.cs
+++ Simulator/PharmacokineticModelFactory.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Reflection;
 using IniUtils;
 
@@ -16,6 +20,16 @@ namespace Simulator
         public double Age { get; set; }
         public bool IsMale { get; set; }
 
+        // パラメータを保持
+        private Dictionary<string, double> _CustomParams = new Dictionary<string, double>();
+
+        private bool _IsCustomParamsComliled = false;
+
+        // Compute関数を呼ぶためだけのオブジェクト
+        private DataTable _ParamTable = new DataTable();
+
+        private Regex _ParamRegex = new Regex("<(.*?)>", RegexOptions.Compiled);
+
         public PharmacokineticModelFactory()
         {
         }
@@ -26,12 +40,15 @@ namespace Simulator
             Stat = stat;
             Age = age;
             IsMale = isMale;
+
+            CompileCustomParams();
+
         }
 
 
         public PharmacokineticModel Create(int modelNumber)
         {
-            var section = Configurations.Config?.Sections[$"model_{modelNumber}"];
+            var section = Configurations.Config.Sections[$"model_{modelNumber}"];
             string name = section.Keys["name"].Value;
             double k10 =Evaluate( section.Keys["k10"].Value);
             double k12 =Evaluate( section.Keys["k12"].Value);
@@ -40,7 +57,6 @@ namespace Simulator
             double k31 =Evaluate( section.Keys["k31"].Value);
             double ke0 =Evaluate( section.Keys["ke0"].Value);
             double v1 = Evaluate(section.Keys["V1"].Value);
-
 
             var model = new PharmacokineticModel(name, k10, k12, k13, k21, k31, ke0, Weight);
             model.V1 = v1;
@@ -52,14 +68,114 @@ namespace Simulator
 
         private double Evaluate(string expression)
         {
+            // 基本的な情報で置換する
+            string replaced = ReplaceProperty(expression);
+
+            // 辞書からカスタムパラメータを取得して置換する
+            if (_IsCustomParamsComliled && _ParamRegex.IsMatch(replaced))
+            {
+                foreach (Match item in _ParamRegex.Matches(replaced))
+                {
+                    if (TryGetParam(item.Groups[1].Value, out var val))
+                    {
+                        replaced = replaced.Replace(item.Value, val.ToString());
+                    }
+                }
+            }
+
+            Type t = Type.GetTypeFromProgID("MSScriptControl.ScriptControl");
+            object obj = Activator.CreateInstance(t);
+            t.InvokeMember("Language", BindingFlags.SetProperty, null, obj, new object[] {"vbscript"});
+            //Eval関数で計算を実行して結果を取得
+            double result = (double) t.InvokeMember("Eval", BindingFlags.InvokeMethod, null, obj, new object[] { replaced });
+            return result;
+        }
+
+        /// <summary>
+        /// プロパティによって置換できるパラメータを置換する
+        /// </summary>
+        /// <param name="expression">置換対象の式</param>
+        /// <returns>置換された式</returns>
+        private string ReplaceProperty(string expression)
+        {
+            // 基本的な情報で置換する
             string replaced = expression;
             replaced = replaced.Replace("<体重>", Weight.ToString());
             replaced = replaced.Replace("<年齢>", Age.ToString());
             replaced = replaced.Replace("<身長>", Stat.ToString());
-
-            var dt = new System.Data.DataTable();
-            return double.Parse(dt.Compute(replaced, "").ToString());
+            return replaced;
         }
+
+        /// <summary>
+        /// 辞書からパラメータを探す
+        /// </summary>
+        /// <param name="key">パラメータ名</param>
+        /// <param name="value">値</param>
+        /// <returns>取得に成功したらTrueを返す</returns>
+        private bool TryGetParam(string key, out double value)
+        {
+            // 先に性別分岐パラメータを探す
+            string suffix = IsMale ? "M" : "F";
+            if (_CustomParams.ContainsKey($"{key}_{suffix}"))
+            {
+                value = _CustomParams[$"{key}_{suffix}"];
+                return true;
+            }
+
+            if (_CustomParams.ContainsKey(key))
+            {
+                value = _CustomParams[key];
+                return true;
+            }
+
+            value = 0;
+            return false;
+        }
+
+        /// <summary>
+        /// カスタムパラメータを読み込む
+        /// </summary>
+        private void CompileCustomParams()
+        {
+            // 依存関係が複雑なパラメータがあるかもしれないので10回まで繰り替えす
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (var param in Configurations.Config.Sections["CustomParams"].GetIniValues())
+                {
+                    if (_CustomParams.ContainsKey(param.KeyName))
+                    {
+                        continue;
+                    }
+
+                    string replaced = ReplaceProperty(param.Value);
+
+                    if (_ParamRegex.IsMatch(replaced))
+                    {
+                        
+                        foreach (Match item in _ParamRegex.Matches(replaced))
+                        {
+                            if (TryGetParam(item.Groups[1].Value, out var val))
+                            {
+                                replaced = replaced.Replace(item.Value, val.ToString());
+                            }
+                        }
+
+                        // 変換してパラメータが残っていなければ完了
+                        if (!_ParamRegex.IsMatch(replaced))
+                        {
+                            _CustomParams.Add(param.KeyName, Evaluate(replaced));
+                        }
+                    }
+                    else
+                    {
+                        _CustomParams.Add(param.KeyName, Evaluate(param.Value));
+                    }
+                }
+            }
+
+            _IsCustomParamsComliled = true;
+        }
+
 
 
         /// <summary>
